396a3d938bd8498de8694a73ae8b6f14ba5fabd6
Jzh <263108911@qq.com>
2020-05-18 17:36:07 +0800

    添加项目文件。

---
+++ PictureCrawler/PictureCrawler.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace PictureCrawler
+{
+    partial class PictureCrawler
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Crawl = new System.Windows.Forms.Button();
+            this.URL = new System.Windows.Forms.TextBox();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SavePath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SavePathLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Crawl
+            // 
+            this.Crawl.Location = new System.Drawing.Point(77, 78);
+            this.Crawl.Name = "Crawl";
+            this.Crawl.Size = new System.Drawing.Size(75, 23);
+            this.Crawl.TabIndex = 0;
+            this.Crawl.Text = "Crawl";
+            this.Crawl.UseVisualStyleBackColor = true;
+            this.Crawl.Click += new System.EventHandler(this.Crawl_Click);
+            // 
+            // URL
+            // 
+            this.URL.Location = new System.Drawing.Point(77, 51);
+            this.URL.Name = "URL";
+            this.URL.Size = new System.Drawing.Size(431, 21);
+            this.URL.TabIndex = 1;
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(29, 117);
+            this.outputText.Multiline = true;
+            this.outputText.Name = "outputText";
+            this.outputText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputText.Size = new System.Drawing.Size(479, 283);
+            this.outputText.TabIndex = 2;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // SavePath
+            // 
+            this.SavePath.Location = new System.Drawing.Point(77, 12);
+            this.SavePath.Name = "SavePath";
+            this.SavePath.Size = new System.Drawing.Size(431, 21);
+            this.SavePath.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(470, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "···";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OpenFilePath_Click);
+            // 
+            // SavePathLabel
+            // 
+            this.SavePathLabel.AutoSize = true;
+            this.SavePathLabel.Location = new System.Drawing.Point(9, 15);
+            this.SavePathLabel.Name = "SavePathLabel";
+            this.SavePathLabel.Size = new System.Drawing.Size(53, 12);
+            this.SavePathLabel.TabIndex = 5;
+            this.SavePathLabel.Text = "保存位置";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "目标地址";
+            // 
+            // PictureCrawler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 449);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SavePathLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SavePath);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.URL);
+            this.Controls.Add(this.Crawl);
+            this.Name = "PictureCrawler";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PictureCrawler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Crawl;
+        private System.Windows.Forms.TextBox URL;
+        private System.Windows.Forms.TextBox outputText;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox SavePath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Label SavePathLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PictureCrawler/PictureCrawler.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using HtmlAgilityPack;
+using mshtml;
+using System.Configuration;
+
+namespace PictureCrawler
+{
+    public partial class PictureCrawler : Form
+    {
+        public PictureCrawler()
+        {
+            InitializeComponent();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Crawl_Click(object sender, EventArgs e)
+        {
+            this.outputText.Text = "";
+            int imagenum = 0;
+
+            List<string> urls = GetURL(URL.Text);
+
+            string path = SavePath.Text;
+
+            int i = 10;
+            foreach (var url in urls)
+            {
+                if (i > 0)
+                {
+                    Task.Run(() =>
+                    {
+                        RealAsyncDownloadImages(url, path, ref imagenum);
+                    });
+                    i--;
+                }
+            }
+
+            //SingleThreadDownloadImages(URL.Text, path);
+
+
+        }
+
+        /// <summary>
+        /// 模拟 同步
+        /// </summary>
+        private void SingleThreadDownloadImages(string URL, string Path)
+        {
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    //调用线程 空转等待。。。
+                    var content = client.GetStringAsync(URL).Result;
+                    var html = new HtmlAgilityPack.HtmlDocument();
+                    html.LoadHtml(content);
+
+                    var imgsrcList = html.DocumentNode.SelectNodes("//img").Select(m => m.Attributes["src"].Value)
+                                                      .ToList();
+
+                    this.outputText.AppendText($"{URL}准备下载:{imgsrcList.Count}个...");
+
+                    for (int i = 0; i < imgsrcList.Count; i++)
+                    {
+                        HttpClient http = new HttpClient();
+                         imgsrcList[i] = http.GetByteArrayAsync(imgsrcList[i]).ToString();
+
+                        this.outputText.AppendText("{i}:开始下载\r\n");
+                        if (imgsrcList[i].Contains(@"//"))
+                        {
+                            try
+                            {
+                                var stream = client.GetStreamAsync(imgsrcList[i]).Result;
+
+                                Image.FromStream(stream).Save(Path + $@"\{Guid.NewGuid().ToString()}.jpg");
+                                this.outputText.AppendText($"{i}:已完成\r\n");
+                            }
+                            catch(Exception ex) {
+                                this.outputText.AppendText($"{i}:{imgsrcList[i]}  下载失败\r\n");
+                            }
+                        }
+                        else
+                        {
+                            try
+                            {
+                                var stream = client.GetStreamAsync(URL + imgsrcList[i]).Result;
+
+                                Image.FromStream(stream).Save(Path + $@"\{Guid.NewGuid().ToString()}.jpg");
+                                this.outputText.AppendText( $"{i}:已完成\r\n");
+                            }
+                            catch (Exception ex)
+                            {
+                                this.outputText.AppendText($"{i}:{imgsrcList[i]}  下载失败\r\n");
+                            }
+                        }
+                    }
+
+                    this.outputText.AppendText($"SingleThreadDownloadImages {URL} 执行结束");
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+
+        /// <summary>
+        /// 模拟爬取 异步
+        /// </summary>
+        private async void AsyncDownloadImages(string URL)
+        {
+
+            using (var client = new HttpClient())
+            {
+                //调用线程 空转等待。。。
+                var content = await client.GetStringAsync(URL);
+                var html = new HtmlAgilityPack.HtmlDocument();
+                html.LoadHtml(content);
+
+                var imgsrcList = html.DocumentNode.SelectNodes("//img").Select(m => m.Attributes["src"].Value)
+                                                  .ToList();
+
+                this.outputText.AppendText($"准备下载:{imgsrcList.Count}个...");
+
+                for (int i = 0; i < imgsrcList.Count; i++)
+                {
+                    this.outputText.AppendText(i.ToString());
+                    if (imgsrcList[i].Contains(@"//"))
+                    {
+                        //调用线程 空转等待。。。
+                        var stream = await client.GetStreamAsync(imgsrcList[i]);
+
+                        Image.FromStream(stream).Save($@"D:\test\{i}.jpg");
+                    }
+                    else
+                    {
+                        //调用线程 空转等待。。。
+                        var stream = await client.GetStreamAsync(URL + imgsrcList[i]);
+
+                        Image.FromStream(stream).Save($@"D:\test\{i}.jpg");
+                    }
+                }
+
+                this.outputText.AppendText("SingleThreadDownloadImages 执行结束");
+            }
+        }
+
+        /// <summary>
+        /// 模拟爬取 真`异步
+        /// </summary>
+        /// <param name="URL">目标地址</param>
+        /// <param name="Path">保存路径</param>
+        private void RealAsyncDownloadImages(string URL, string Path, ref int imagenum)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    List<Task> tasks = new List<Task>();
+
+                    if (URL.Substring(0, 2) == @"//")
+                    {
+                        URL = "http:" + URL;
+                    }
+
+                    var content = client.GetStringAsync(URL).ConfigureAwait(false).GetAwaiter().GetResult();
+                    var html = new HtmlAgilityPack.HtmlDocument();
+                    html.LoadHtml(content);
+
+                    var imgsrcList = html.DocumentNode.SelectNodes("//img").Select(m => m.Attributes["src"].Value).Distinct()
+                                                    .ToList();
+
+
+                    //var html = new HtmlAgilityPack.HtmlDocument();
+                    //List<string> imgsrcList = new List<string>();
+                    //var content = client.GetStringAsync(URL).ContinueWith(c =>
+                    //{
+                    //    try
+                    //    {
+                    //        string contentstring = c.ConfigureAwait(false).GetAwaiter().GetResult();
+                    //        html.LoadHtml(contentstring);
+                    //        imgsrcList = html.DocumentNode.SelectNodes("//img").Select(m => m.Attributes["src"].Value).Distinct()
+                    //                               .ToList();
+                    //        Invoke((Action)(() =>
+                    //        {
+                    //            this.outputText.AppendText($"{URL}准备下载:{imgsrcList.Count}个...\r\n");
+                    //        }));
+                    //    }
+                    //    catch { }
+                    //});
+                    //tasks.Add(content);
+
+                    Invoke((Action)(() =>
+                    {
+                        this.outputText.AppendText($"{URL}准备下载:{imgsrcList.Count}个...\r\n");
+                    }));
+
+
+                    for (int i = 0; i < imgsrcList.Count; i++)
+                    {
+                        Interlocked.Increment(ref imagenum);
+                        var thisuri = imgsrcList[i];
+                        var num = imagenum;
+                        this.outputText.AppendText($"{num}:开始下载\r\n");
+                        if (imgsrcList[i].Contains(@"//"))
+                        {
+                            var stream = client.GetStreamAsync(imgsrcList[i]).ContinueWith(p =>
+                            {
+                                try
+                                {
+                                    var a = p.Result;
+                                    Image.FromStream(a).Save(Path + $@"\{Guid.NewGuid().ToString()}.jpg");
+                                    this.outputText.AppendText( $"{num}:已完成\r\n");
+                                }
+                                catch (Exception ex)
+                                {
+                                    this.outputText.AppendText($"{num}:{thisuri}  下载失败 : {ex.Message}\r\n");
+                                }
+                            });
+                            tasks.Add(stream);
+                        }
+                        else
+                        {
+                            var stream = client.GetStreamAsync(URL + imgsrcList[i]).ContinueWith(p =>
+                            {
+                                try
+                                {
+                                    var a = p.Result;
+                                    Image.FromStream(a).Save(Path + $@"\{Guid.NewGuid().ToString()}.jpg");
+                                    this.outputText.AppendText($"{num}:已完成\r\n");
+                                }
+                                catch (Exception ex)
+                                {
+                                    this.outputText.AppendText($"{num}:{URL}  :  { thisuri}  下载失败 : {ex.Message}\r\n");
+                                }
+                            });
+                            tasks.Add(stream);
+                        }
+
+                    }
+                    Task.WaitAll(tasks.ToArray());
+                    Invoke((Action)(() =>
+                    {
+                        this.outputText.AppendText( $"{URL}: 执行结束\r\n");
+                    }));
+                }
+                catch (Exception ex)
+                {
+                    //this.outputText.AppendText(ex.Message + "\r\n");
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// 爬取 某页面下的URL链接
+        /// </summary>
+        private List<string> GetURL(string URL)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var content = client.GetStringAsync(URL).ConfigureAwait(false).GetAwaiter().GetResult();
+                    var html = new HtmlAgilityPack.HtmlDocument();
+                    html.LoadHtml(content);
+
+                    var URLList = html.DocumentNode.SelectNodes("//a").Select(m => m.Attributes["href"].Value)
+                                                    ?.ToList();
+                    URLList.Insert(0, URL);
+                    URLList = URLList.Where(u => u.Contains("#") != true).Distinct().ToList();
+                    this.outputText.AppendText($"获取URL:{URLList.Count}个...\r\n");
+
+
+                    return URLList;
+                }
+                catch(Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+
+        private void OpenFilePath_Click(object sender, EventArgs e)
+        {
+            if (ConfigurationManager.AppSettings["FolderSelectedPath"] != "")
+            {
+                folderBrowserDialog.SelectedPath = ConfigurationManager.AppSettings["FolderSelectedPath"];
+            }
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                SavePath.Text = folderBrowserDialog.SelectedPath;
+                //窗体关闭时，获取文件夹对话框的路径写入配置文件中
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                config.AppSettings.Settings["FolderSelectedPath"].Value = SavePath.Text;
+                //一定要记得保存，写不带参数的config.Save()也可以
+                config.Save(ConfigurationSaveMode.Modified);
+                //刷新，否则程序读取的还是之前的值（可能已装入内存）
+                System.Configuration.ConfigurationManager.RefreshSection("appSettings");
+
+            }
+        }
+    }
+}
